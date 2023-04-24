01bb2c9aefbb45240178db8be23d468b88ed334a
Jay-Khakim <mgmediajay@gmail.com>
2023-04-25 03:21:40 +0900

    React and Redux forms

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -9,7 +9,11 @@ const UsersList = () => {
 		isSuccess,
 		isError, 
 		error 
-	} = useGetUsersQuery()
+	} = useGetUsersQuery(undefined, {
+		pollingInterval: 60000,
+		refetchOnFocus: true,
+		refetchOnMountOrArgChange: true
+	})
 	// console.log(users)
 	let content
 
